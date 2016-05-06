b688337835c6193a37bf840c5c1923507977b129
Karthik128 <dr.m.karthiik@gmail.com>
2016-05-06 13:54:16 +0530

    ReverseString

---
+++ src/ReverseString.java
@@ -0,0 +1,19 @@
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name= "marihbA payhsaaK";
+		
+		String reverse = new StringBuffer(name).reverse().toString();
+		
+		System.out.println("String Before Reverse:");
+		System.out.println("\n"+name);
+		
+		System.out.println("\nString After Reverse:");
+		System.out.println("\n"+reverse);
+
+	}
+
+}
